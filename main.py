81d92201a4807895948c623204dae8e3ccff653b
pauldg <pauldegeest1@gmail.com>
2023-11-18 22:56:08 +0100

    add destination type

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from typing import Union
+from typing import List
 
 from fastapi import FastAPI
+
 from pydantic import BaseModel
 
+from tpv.core.entities import Destination
+
+
+from closest_location import closest_destination
+objectstore_loc_path = "tests/fixtures/object_store_locations.yml"
+selected_object_store = "object_store_australia"
+# print(type(candidate_destinations))
+print(candidate_destinations[0])
+# print(candidate_destinations)
+final_destinations = closest_destination(candidate_destinations, objectstore_loc_path, selected_object_store)
 
 # object_store_italy_S3_01:
 #   latitude: 50.0689816
@@ -25,16 +36,25 @@ from pydantic import BaseModel
 #       longitude: 19.9070188
 
 
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
+# Pydantic model for request input
+class DestinationRequest(BaseModel):
+    destinations: List[Destination]
+
+
+# Pydantic model for response
+class ProcessedResult(BaseModel):
+    processed_destinations: List[str]
 
 
 app = FastAPI()
 
+@app.post("/process_destinations", response_model=ProcessedResult)
+async def process_destinations(destinations_request: DestinationRequest):
+    processed_destinations = []
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+    for destination in destinations_request.destinations:
+        # Example processing: extracting destination name and runner
+        processed_info = f"Runner: {destination.runner}, Destination Name: {destination.dest_name}"
+        processed_destinations.append(processed_info)
+
+    return {"processed_destinations": processed_destinations}
